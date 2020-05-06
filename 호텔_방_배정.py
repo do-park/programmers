90b90bca6744e37663e9acc0d2180616a211ef49
박도희 <dohee.pa@gmail.com>
2020-05-06 21:52:54 +0900

    May 6. Done

--- 호텔_방_배정.py
+++ 호텔_방_배정.py
@@ -1,23 +1,21 @@
 # 2019 카카오 개발자 겨울 인턴십
 # 호텔 방 배정
-# 정확성: 78.8 , 효율성: 0.0, 합계 78.8/100
+# 정확성: 78.8 , 효율성: 18.2, 합계 97.0/100
+
+def empty(x, rooms):
+    if x not in rooms:
+        rooms[x] = x + 1
+        return x
+    p = empty(rooms[x], rooms)
+    rooms[x] = p + 1
+    return p
 
 def solution(k, room_number):
     answer = []
-    hotel = [False] * (k + 1)
-    for room in room_number:
-        if not hotel[room]:
-            hotel[room] = True
-            answer.append(room)
-        else:
-            i = 1
-            while True:
-                if not hotel[room + i]:
-                    hotel[room + i] = True
-                    answer.append(room + i)
-                    break
-                else:
-                    i += 1
+    rooms = dict()
+    for i in room_number:
+        book = empty(i, rooms)
+        answer.append(book)
     return answer
 
 print(solution(10, [1, 3, 4, 1, 3, 1]))
