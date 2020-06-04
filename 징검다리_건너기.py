de6b11081b7f48f50ca8e631451280c37b31bd5d
박도희 <dohee.pa@gmail.com>
2020-06-04 11:52:33 +0900

    Update 징검다리_건너기.py
    Jun 4. 이분 탐색

--- 징검다리_건너기.py
+++ 징검다리_건너기.py
@@ -1,6 +1,6 @@
 # 2019 카카오 개발자 겨울 인턴십
 # 징검다리 건너기
-# 정확성 61.5(24/25), 효율성 35.9(10/10), 합계 97.4
+# 정확성 61.5(25/25), 효율성 35.9(10/10), 합계 100
 
 def can(stones, k, mid):
     count = 0
@@ -15,7 +15,7 @@ def can(stones, k, mid):
 
 def solution(stones, k):
     left = 1
-    right = max(stones)
+    right = max(stones) + 1
     while left < (right - 1):
         mid = (left + right) // 2
         if can(stones, k, mid):
@@ -23,9 +23,3 @@ def solution(stones, k):
         else:
             right = mid
     return left
-
-
-stones = [2, 4, 5, 3, 2, 1, 4, 2, 5, 1]
-k = 3
-result = 3
-print(solution(stones, k))
