d510275e54165db0e6deaf5c98849d6758cf49fe
박도희 <dohee.pa@gmail.com>
2020-12-05 17:19:11 +0900

    CREATE | 문자열 처리

---
+++ 영어 끝말잇기.py
@@ -0,0 +1,16 @@
+def solution(n, words):
+    cnt = len(words)
+    done = {words[0]}
+    for turn in range(1, cnt):
+        if words[turn] in done:
+            # return 번호, 차례
+            return [turn % n + 1, turn // n + 1]
+        if words[turn - 1][-1] != words[turn][0]:
+            return [turn % n + 1, turn // n + 1]
+        done.add(words[turn])
+    return [0, 0]
+
+
+print(solution(3, 	['tank', 'kick', 'know', 'wheel', 'land', 'dream', 'mother', 'robot', 'tank']))
+print(solution(5, 	['hello', 'observe', 'effect', 'take', 'either', 'recognize', 'encourage', 'ensure', 'establish', 'hang', 'gather', 'refer', 'reference', 'estimate', 'executive']))
+print(solution(2, ['hello', 'one', 'even', 'never', 'now', 'world', 'draw']))
